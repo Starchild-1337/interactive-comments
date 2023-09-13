5a666490dfeae7a39b462514d364df9e66a3d551
Starchild-1337 <beqamamulia@gmail.com>
2023-09-13 17:27:39 +0400

    testing

--- src/index.js
+++ src/index.js
@@ -2,25 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+} from "react-router-dom";
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
 import ErrorPage from "./pages/404";
 import { AppProvider } from "./context";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     errorElement: <ErrorPage />, // <-- root error component
     children: [
       {
-        path: "/interactive-comments",
+        path: "/",
         children: [
           {
             index: true,
             element: <Feed />,
           },
           {
-            path: "/interactive-comments/login",
+            path: "/login",
             element: <Login />,
           },
         ],
